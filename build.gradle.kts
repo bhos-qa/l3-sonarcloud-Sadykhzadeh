bd7ba522b10e83e28fc39154ba51668a7e72f900
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2023-09-25 11:49:39 +0400

    build.gradle.kts fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    jacoco
     id("org.sonarqube") version "4.3.1.3277"
 }
 
@@ -27,11 +28,40 @@ application {
     mainClass.set("MainKt")
 }
 
+
+jacoco {
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("reportsJaCoCo"))
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+    reportsDirectory.set(layout.buildDirectory.dir("JaCoCoTest"))
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "bhos-qa_l3-sonarcloud-Sadykhzadeh")
         property("sonar.organization", "bhos-qa")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/test-results/test/TEST-PermissionManagerTest.xml")
+        property("sonar.scm.forceReloadAll", "true")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.jacoco.reportPath", "build/JaCoCoTest/test")
+        property("sonar.junit.reportsPath", "build/test-results/test")
     }
 }
