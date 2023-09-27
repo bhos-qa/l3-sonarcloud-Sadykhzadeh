36f66de43bf7f7eaad01ec2f92036753d7636677
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2023-09-27 10:50:55 +0400

    remove Main.kt

--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    val permissionManager = PermissionManager()
-    permissionManager.setPermissionLevel(PermissionLevel.ADMIN)
-    println("Current permission level: ${permissionManager.getPermissionLevelName()}")
-}
